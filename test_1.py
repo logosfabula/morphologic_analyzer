57e034dbcdbd40ffdc00a79b642bdc68fa35dbae
logosfabula <logosfabula@gmail.com>
2018-12-01 19:51:03 +0100

    incapsulated support function for tests

--- test_1.py
+++ test_1.py
@@ -1,31 +1,28 @@
 # TEST PRECONDITIONS: no optional command-line arguments, empty 'removed_words.txt' #
+## check ./test_support/ for support functions
 
 import unittest
 import compounds_splitter
-
-# utilities
-
-def prepare_result(results):
-    return str(sorted(results)).strip('][').replace("'","").replace("  "," ").strip()
+import test_support.support
 
 # TEST CASES
 
 def test_sunflower():
     results = []
     compounds_splitter.split_compound(compounds_splitter.english_words, "sunflower", '', results)
-    assert "sun flower, sunflower" == prepare_result(results)
+    assert "sun flower, sunflower" == test_support.support.prepare_result(results)
 
 def test_cockroach():
     results = []
     compounds_splitter.split_compound(compounds_splitter.english_words, "cockroach", '', results)
-    assert "cock roach, cockroach" == prepare_result(results)
+    assert "cock roach, cockroach" == test_support.support.prepare_result(results)
 
 def test_pleasecretin():
     results = []
     compounds_splitter.split_compound(compounds_splitter.english_words, "dropin", '', results)
-    assert "drop in" == prepare_result(results)
+    assert "drop in" == test_support.support.prepare_result(results)
 
 def test_doorin():
     results = []
     compounds_splitter.split_compound(compounds_splitter.english_words, "doorin", '', results)
-    assert "do or in, door in" == prepare_result(results)
+    assert "do or in, door in" == test_support.support.prepare_result(results)
---
+++ test_support/support.py
@@ -0,0 +1,4 @@
+# support functions for tests
+
+def prepare_result(results):
+    return str(sorted(results)).strip('][').replace("'","").replace("  "," ").strip()
